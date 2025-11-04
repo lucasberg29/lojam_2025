55c35343f0224c664893288344405a202271569e
lucasberg29 <lucasberg29@gmail.com>
2025-11-03 22:45:58 -0500

    added running animation with it's implementation

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
 const PLAYER_SPEED := 130.0
+const PLAYER_RUNNING_SPEED := 300
 const ACCELERATION := 800.0
 const FRICTION := 600.0
-const JUMP_VELOCITY := -300.0
+const JUMP_VELOCITY := -400.0
+const RUNNING_JUMP_VELOCITY := -500.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var current_velocity = 0
 var on_floor = false
+var is_shift_down = false
 var direction: float
 
 var animation_controller:PlayerAnimation
@@ -18,19 +21,32 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	update_movement(delta)
-	animation_controller.update_animation(direction, current_velocity, on_floor)
+	animation_controller.update_animation(direction, current_velocity, on_floor, is_shift_down)
 
 func update_movement(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		on_floor = false
 	else:
 		on_floor = true
 		velocity.y = 0.0
+
+	is_shift_down = Input.is_action_pressed("run")
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		if is_shift_down:
+			velocity.y = RUNNING_JUMP_VELOCITY
+		else:
+			velocity.y = JUMP_VELOCITY
 
 	direction = Input.get_axis("move_left", "move_right")
 
 	if direction != 0:
 		var target_speed := direction * PLAYER_SPEED
+		
+		if is_shift_down:
+			target_speed = direction * PLAYER_RUNNING_SPEED
+		
 		velocity.x = move_toward(velocity.x, target_speed, ACCELERATION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
--- scripts/player/player_animation.gd
+++ scripts/player/player_animation.gd
@@ -19,24 +19,33 @@ func turn_left():
 func turn_right():
 	sprite.flip_h = true;
 
-func update_animation(direction:float, velocity: Vector2, on_floor: bool):
+func update_animation(direction:float, velocity: Vector2, on_floor: bool, _is_shift_down: bool):
 	if direction != 0:
 		update_direction(velocity)
 
 	if not on_floor:
-		clear_conditions()
-		animation_tree.set("parameters/conditions/jump", true)
+		if velocity.y < 0:
+			clear_conditions()
+			animation_tree.set("parameters/conditions/jump_up", true)
+		elif velocity.y > 0:
+			clear_conditions()
+			animation_tree.set("parameters/conditions/jump_down", true)
 	elif abs(velocity.x) < 10:
 		clear_conditions()
 		animation_tree.set("parameters/conditions/idle", true)
 	else:
 		clear_conditions()
-		animation_tree.set("parameters/conditions/walk", true)
+		if _is_shift_down:
+			animation_tree.set("parameters/conditions/run", true)
+		else:
+			animation_tree.set("parameters/conditions/walk", true)
 
 func clear_conditions():
 		animation_tree.set("parameters/conditions/idle", false)
 		animation_tree.set("parameters/conditions/walk", false)
-		animation_tree.set("parameters/conditions/jump", false)
+		animation_tree.set("parameters/conditions/run", false)
+		animation_tree.set("parameters/conditions/jump_up", false)
+		animation_tree.set("parameters/conditions/jump_down", false)
 
 func update_direction(velocity: Vector2):
 	if velocity.x > 0:
