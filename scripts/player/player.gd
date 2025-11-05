088d5462fa594ecd8fa17dabfc58c10b12792e31
lucasberg29 <lucasberg29@gmail.com>
2025-11-05 18:29:15 -0500

    added Mario thing for variable jump

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,14 +4,18 @@ const PLAYER_SPEED := 130.0
 const PLAYER_RUNNING_SPEED := 300
 const ACCELERATION := 800.0
 const FRICTION := 600.0
-const JUMP_VELOCITY := -400.0
-const RUNNING_JUMP_VELOCITY := -500.0
+const JUMP_VELOCITY := -350.0
+const RUNNING_JUMP_VELOCITY := -450.0
+var gravity_modifier = 1.0
+
+const JUMP_INCREMENT = 5.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var current_velocity = 0
 var on_floor = false
 var is_shift_down = false
+var is_jump_down = false
 var direction: float
 
 var animation_controller:PlayerAnimation
@@ -25,12 +29,13 @@ func _physics_process(delta: float) -> void:
 
 func update_movement(delta: float):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta / gravity_modifier
 		on_floor = false
 	else:
 		on_floor = true
 		velocity.y = 0.0
-
+	
+	is_jump_down = Input.is_action_pressed("jump")
 	is_shift_down = Input.is_action_pressed("run")
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -39,6 +44,9 @@ func update_movement(delta: float):
 		else:
 			velocity.y = JUMP_VELOCITY
 
+	if not is_on_floor() and velocity.y < 0 and is_jump_down:
+		velocity.y -= JUMP_INCREMENT
+
 	direction = Input.get_axis("move_left", "move_right")
 
 	if direction != 0:
@@ -46,6 +54,9 @@ func update_movement(delta: float):
 		
 		if is_shift_down:
 			target_speed = direction * PLAYER_RUNNING_SPEED
+		
+		if is_jump_down:
+			target_speed *= 1.5
 		
 		velocity.x = move_toward(velocity.x, target_speed, ACCELERATION * delta)
 	else:
